9594a72cef883297cfd60dd3cb2258ec95958864
Satyam Sinha <satyamsinha340@gmail.com>
2025-02-14 23:31:03 +0530

    Updated files

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Hero from './landing_page/home/Hero'
 import Awards from "./landing_page/home/Awards.jsx";
+import Stats from './landing_page/home/Stats.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Hero />
+      <Stats />
       <Awards />
   </StrictMode>,
 )
