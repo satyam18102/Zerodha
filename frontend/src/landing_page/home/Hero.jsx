e475400e7b5f9555d391c0c06bd8538dd4fbef84
Satyam Sinha <satyamsinha340@gmail.com>
2025-02-14 23:05:23 +0530

    Updated files

---
+++ frontend/src/landing_page/home/Hero.jsx
@@ -0,0 +1,12 @@
+
+export default function Hero() {
+    return (
+        <div className="hero">
+            <div className="hero__content">
+                <h1 className="hero__title">Welcome to the <span className="hero__title--highlight">MERN</span> Stack</h1>
+                <p className="hero__description">A simple and easy way to get started with the MERN stack</p>
+                <a href="#about" className="hero__cta">Learn More</a>
+            </div>
+        </div>
+    );
+}
