af93a8904ed650962375834c5496835b597c07cd
Satyam Sinha <satyamsinha340@gmail.com>
2025-02-16 19:37:21 +0530

    Creating Home Page

--- frontend/src/landing_page/home/Education.jsx
+++ frontend/src/landing_page/home/Education.jsx
@@ -3,11 +3,11 @@ import './Home.css'
 export default function Education() {
     return (
         <>
-            <div className="flex-2">
+            <div className="flex-2 ">
                 <div className="img">
                     <img src='/education.svg' />
                 </div>
-                <div className="stat-cont ">
+                <div className="stat-cont small ">
                     <div className="mb-5">
                     <h2 className='mb-4'>Free and open market education</h2>
                     <p>Varsity, the largest online stock market education book in the world covering everything from the basics to advanced trading.</p>
