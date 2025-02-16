2094ed604da61819faee05520db76764587c40e0
Satyam Sinha <satyamsinha340@gmail.com>
2025-02-16 12:57:42 +0530

    Creating Home Page

--- frontend/src/landing_page/home/Pricing.jsx
+++ frontend/src/landing_page/home/Pricing.jsx
@@ -1,9 +1,15 @@
-
+import './Home.css'
 
 export default function Pricing () {
     return (
         <>
-            <h1>Unbeatable Pricing</h1>
+            <div className="container flex-2 ">
+                <div className="stat-cont">
+                    <h3 className='mb-4'>Unbeatable Pricing</h3>
+                    <p>We pioneered the concept of discount broking and price transparency in India. Flat fees and no hidden charges.</p>
+                    <a href='https://zerodha.com/charges/'>See pricing</a>
+                </div>
+            </div>
         </>
     );
 }
