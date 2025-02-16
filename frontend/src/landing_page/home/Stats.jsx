d52bbc842b3d41d4294595c54ba6dd282cd3e3c5
Satyam Sinha <satyamsinha340@gmail.com>
2025-02-16 19:42:43 +0530

    Creating Home Page

--- frontend/src/landing_page/home/Stats.jsx
+++ frontend/src/landing_page/home/Stats.jsx
@@ -7,13 +7,16 @@ export default function Stats() {
                 <div className="stat-cont small">
                     <h2>Trust with confidence</h2>
                     <h3 className='mt-5'>Customer-first always</h3>
-                    <p className='mb-5 text-muted'>That's why 1.5+ crore customers trust Zerodha with ₹4.5+ lakh crores of equity investments and contribute to 15% of daily retail exchange volumes in India.</p>
+                    <p className='mb-5 text-muted'>That&#39;s why 1.5+ crore customers trust Zerodha with ₹4.5+ lakh crores of equity investments and contribute to 15% of daily retail exchange volumes in India.</p>
                     <h3>No spam or gimmics</h3>
-                    <p className='mb-5 text-muted'>No gimmicks, spam, "gamification", or annoying push notifications. High quality apps that you use at your pace, the way you like.</p>
+                    <p className='mb-5 text-muted'>No gimmicks, spam, "gamification&#39;, or annoying push notifications. High quality apps that you use at your pace, the way you like.</p>
                     <h3>The Zerodha universe</h3>
                     <p className='mb-5 text-muted'>Not just an app, but a whole ecosystem. Our investments in 30+ fintech startups offer you tailored services specific to your needs.</p>
                     <h3>Do better with money</h3>
-                    <p className='mb-5 text-muted'>With initiatives like Nudge and Kill Switch, we don't just facilitate transactions, but actively help you do better with your money.</p>
+                    <p className='mb-5 text-muted'>With initiatives like
+                        <a href='https://support.zerodha.com/category/trading-and-markets/kite-features/nudges/articles/what-is-nudge'> Nudge </a> and
+                        <a href='https://support.zerodha.com/category/console/profile/killswitch/articles/what-is-the-kill-switch' > Kill Switch</a>
+                        , we don&#39;t just facilitate transactions, but actively help you do better with your money.</p>
                 </div>
                 <div className="img text-center">
                     <img src='/ecosystem.png' alt='Largest Broker' style={{ width: '100%' }} />
