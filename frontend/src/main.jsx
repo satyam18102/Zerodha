f8f0e67c8d57fe7d416fa0583265b6c4e8703dca
Satyam Sinha <satyamsinha340@gmail.com>
2025-02-17 18:32:19 +0530

    Setting Routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,10 @@
 import { createRoot } from 'react-dom/client'
 import HomePage from './landing_page/home/HomePage.jsx'
 import Signup from './landing_page/signup/Signup.jsx'
-
+import About from './landing_page/about/About.jsx'
+import Product from './landing_page/products/Product.jsx'
+import Pricing from './landing_page/pricing/Pricing.jsx'
+import Support from './landing_page/support/Support.jsx'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import './index.css'
 
@@ -12,10 +15,10 @@ createRoot(document.getElementById('root')).render(
       <Routes>
           <Route path="/" Component={HomePage}/>
           <Route path="/signup" Component={Signup} />
-          <Route path="/about" Component={Signup} />
-          <Route path="/products" Component={Signup} />
-          <Route path="/pricing" Component={Signup} />
-          <Route path="/support" Component={Signup} />
+          <Route path="/about" Component={About} />
+          <Route path="/products" Component={Product} />
+          <Route path="/pricing" Component={Pricing} />
+          <Route path="/support" Component={Support} />
       </Routes>
   </BrowserRouter>
 )
