155ebcc3de1ba5091569484a739ca1811487b771
Satyam Sinha <satyamsinha340@gmail.com>
2025-02-17 21:07:30 +0530

    Creating About page

--- frontend/src/landing_page/about/Hero.jsx
+++ frontend/src/landing_page/about/Hero.jsx
@@ -1,8 +1,14 @@
+import './About.css'
 
 export default function Hero () {
     return (
-        <div className="hero">
-            <h1>Hero </h1>
-        </div>
+        <>
+            <div className="main text-center border-bottom">
+                <div>
+                    <h2>We pioneered the discount broking model in India.</h2>
+                    <h2>Now, we are breaking ground with our technology.</h2>
+                </div>
+            </div>
+        </>
     );
 }
