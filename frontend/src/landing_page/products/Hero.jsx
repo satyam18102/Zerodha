eeafc86798c7794d8df70a11d25a1f6acd8fc1a6
Satyam Sinha <satyamsinha340@gmail.com>
2025-02-17 21:07:20 +0530

    Creating Signup page

--- frontend/src/landing_page/products/Hero.jsx
+++ frontend/src/landing_page/products/Hero.jsx
@@ -1,9 +1,13 @@
-
+import './Product.css'
 
 export default function Hero () {
     return (
         <>
-            <h1>Hero </h1>
+            <div className=" main text-center border-bottom">
+                <h1>Zerodha Products</h1>
+                <p>Sleek, modern, and intuitive trading platforms</p>
+                <sapn>Check out our <a href='https://zerodha.com/investments'> investment offerings</a></sapn>
+            </div>
         </>
     );
 }
--- frontend/src/landing_page/signup/Signup.jsx
+++ frontend/src/landing_page/signup/Signup.jsx
@@ -1,8 +1,23 @@
 
+
 export default function Signup() {
     return (
         <>
-            <h1>Signup</h1>
+            <div className="main">
+                <div className="text-center ">
+                    <h1>Open a free demat & trading account online</h1>
+                    <p>Start investing brokerage free and join a community of 1.5+ crore investors and traders</p>
+                </div>
+                <div className="flex margin">
+                    <div className="text-center img ">
+                        <img src='https://signup.zerodha.com/assets/landing-DQ76ex-B.svg' style={{ width: '90%' }} alt="Sign up" />
+                    </div>
+                    <div className="img-cont m-5 ">
+                        <h3>Signup now</h3>
+                        <p className='text-muted'>Or track your existing application</p>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
