1e7a2bfd8727ad25b1682e379f0ab21e95c30ac8
Satyam Sinha <satyamsinha340@gmail.com>
2025-02-17 18:27:46 +0530

    Creating Home Page

---
+++ frontend/src/landing_page/signup/Signup.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../Navbar.jsx";
+import Footer from "../Footer.jsx";
+
+export default function Signup() {
+    return (
+        <>
+            <Navbar />
+            <h1>Signup</h1>
+            <Footer />
+        </>
+    );
+}
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,21 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import HomePage from './landing_page/home/HomePage.jsx'
+import Signup from './landing_page/signup/Signup.jsx'
+
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import './index.css'
 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-      <HomePage />
-  </StrictMode>,
+  <BrowserRouter>
+      <Routes>
+          <Route path="/" Component={HomePage}/>
+          <Route path="/signup" Component={Signup} />
+          <Route path="/about" Component={Signup} />
+          <Route path="/products" Component={Signup} />
+          <Route path="/pricing" Component={Signup} />
+          <Route path="/support" Component={Signup} />
+      </Routes>
+  </BrowserRouter>
 )
