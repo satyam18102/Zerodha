202c40a5fb69e88fb432cb48c4dc890c0f646da2
Satyam Sinha <satyamsinha340@gmail.com>
2025-02-18 20:53:06 +0530

    Creating About section

--- frontend/src/landing_page/about/Hero.jsx
+++ frontend/src/landing_page/about/Hero.jsx
@@ -3,8 +3,8 @@ import './About.css'
 export default function Hero () {
     return (
         <>
-            <div className="main text-center border-bottom">
-                <div>
+            <div className="main text-center ">
+                <div className='border-bottom padding'>
                     <h2>We pioneered the discount broking model in India.</h2>
                     <h2>Now, we are breaking ground with our technology.</h2>
                 </div>
