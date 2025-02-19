8444d305d4ce2319efe59ab732a3d03da4cbc7df
Satyam Sinha <satyamsinha340@gmail.com>
2025-02-20 01:52:12 +0530

    Creating products Section

--- frontend/src/landing_page/products/Universe.jsx
+++ frontend/src/landing_page/products/Universe.jsx
@@ -3,9 +3,20 @@
 export default function Universe () {
     return (
         <>
-            <div className='text-center margin-top'>
-                <h2>The Zerodha Universe</h2>
-                <p>Extend your trading and investment experience even further with our partner platforms</p>
+            <div className='container mt-5 margin-top'>
+                <div className="row">
+                    <h2 className='text-center' >The Zerodha Universe</h2>
+                    <p className='text-center' >Extend your trading and investment experience even further
+                        with our partner platforms
+                    </p>
+                    <div className="col-4 p-3">
+                        <img src='smallcaseLogo.png' alt='Logo' />
+                        <p className='text-small  text-muted' >Our asset management
+                        </p>
+                    </div>
+                    <div className="col-4"></div>
+                    <div className="col-4"></div>
+                </div>
             </div>
         </>
     );
