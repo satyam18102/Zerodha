64bc2825c1224339f2068ba322f406dc0fde423e
Satyam Sinha <satyamsinha340@gmail.com>
2025-02-19 18:27:15 +0530

    Creating Support section

--- frontend/src/landing_page/support/Hero.jsx
+++ frontend/src/landing_page/support/Hero.jsx
@@ -1,10 +1,9 @@
+import './Support.css'
 
 export default function Hero (){
     return (
         <>
-            <div className="text-center mt-5">
-                <h1 className='text-center mt-5 ' >Hero</h1>
-            </div>
+            <h1 className='text-center mt-5 ' >Hero</h1>
         </>
     );
 }
