230d2f94d53c15fc6df1f97348bc86eddc27d75d
Satyam Sinha <satyamsinha340@gmail.com>
2025-02-20 01:20:02 +0530

    Creating products Section

--- frontend/src/landing_page/products/LeftSection.jsx
+++ frontend/src/landing_page/products/LeftSection.jsx
@@ -1,9 +1,27 @@
+import './Product.css'
 
-
-export default function LeftSection () {
+export default function LeftSection ({imageURL,productName,productDescription,tryDemo,learnMore,googlePlay,appStore}) {
     return (
         <>
-            <h1>Left </h1>
+            <div className='flex margin-top' >
+                <div className='leftImg' >
+                    <img src={imageURL} alt={productName} />
+                </div>
+                <div className='leftImg-cont' >
+                    <h2>{productName}</h2>
+                    <p>{productDescription}</p>
+                    <div className='flex' >
+                        <div>
+                            <a href={tryDemo} >Try demo</a><br/>
+                            <a href={googlePlay} ><img src='googlePlayBadge.svg' /></a>
+                        </div>
+                        <div>
+                            <a href={learnMore} >Learn more</a><br/>
+                            <a href={appStore} ><img src='appstoreBadge.svg' /></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
--- frontend/src/landing_page/products/Product.jsx
+++ frontend/src/landing_page/products/Product.jsx
@@ -8,8 +8,36 @@ export default function Product () {
     return (
         <>
             <Hero />
-            <LeftSection />
+            <LeftSection
+                imageURL='kite.png' productName='Kite'
+                productDescription='Our ultra-fast flagship trading platform with streaming market data,
+                 advanced charts, an elegant UI, and more. Enjoy the Kite experience seamlessly on your
+                 Android and iOS devices.'
+                tryDemo='https://kite-demo.zerodha.com/'
+                learnMore='https://zerodha.com/products/kite'
+                googlePlay='https://play.google.com/store/apps/details?id=com.zerodha.kite3'
+                appStore='https://apps.apple.com/in/app/kite-zerodha/id1449453802'
+                />
             <RightSection />
+            <LeftSection
+                imageURL='coin.png' productName='Coin'
+                productDescription='Our ultra-fast flagship trading platform with streaming market data, advanced
+                charts, an elegant UI, and more. Enjoy the Kite experience seamlessly on your Android and iOS devices.'
+                tryDemo='https://coin.zerodha.com/'
+                learnMore='https://coin.zerodha.com/'
+                googlePlay='https://play.google.com/store/apps/details?id=com.zerodha.coin'
+                appStore='https://apps.apple.com/in/app/coin-by-zerodha/id1392892554'
+            />
+            <RightSection/>
+            <LeftSection
+                imageURL='varsity.png' productName='Varsity mobile'
+                productDescription='An easy to grasp, collection of stock market lessons with in-depth coverage
+                and illustrations. Content is broken down into bite-size cards to help you learn on the go.'
+                tryDemo='https://coin.zerodha.com/'
+                learnMore='https://coin.zerodha.com/'
+                googlePlay='https://play.google.com/store/apps/details?id=com.zerodha.varsity'
+                appStore='https://apps.apple.com/in/app/coin-by-zerodha/id1392892554'
+            />
             <Universe />
         </>
     );
