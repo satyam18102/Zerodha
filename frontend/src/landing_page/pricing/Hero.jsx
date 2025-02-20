220164ab4b17de72d3ed7b1529b07a720b520ccc
Satyam Sinha <satyamsinha340@gmail.com>
2025-02-20 11:11:00 +0530

    Adjusting style

--- frontend/src/landing_page/pricing/Hero.jsx
+++ frontend/src/landing_page/pricing/Hero.jsx
@@ -3,7 +3,7 @@ import './pricing.css'
 export default function Hero () {
     return (
         <>
-            <h2 className='mt-5 text-center' >Charges </h2>
+            <h2 className='mt-5 margin-top text-center' >Charges </h2>
             <p>List of all charges and taxes</p>
         </>
     );
