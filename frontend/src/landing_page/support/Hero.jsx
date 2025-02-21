50b9aec348eb2df19dbbf9f7f0f2ac7ed485544b
Satyam Sinha <satyamsinha340@gmail.com>
2025-02-21 20:48:41 +0530

    Creating Support Section

--- frontend/src/landing_page/support/Hero.jsx
+++ frontend/src/landing_page/support/Hero.jsx
@@ -4,7 +4,7 @@ export default function Hero (){
     return (
         <>
             <div className="bg-blue " >
-                <div className='margin-t flex ' >
+                <div className='margin-t flex' >
                     <div className='img '>
                         <h5 className='mb-5' >Support Portal</h5>
                         <h5>Search for an answer or browse help topics to create a ticket</h5>
