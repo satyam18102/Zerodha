4952d270b72bca2f921667fa27e45ada2e0143f9
Satyam Sinha <satyamsinha340@gmail.com>
2025-02-21 20:34:53 +0530

    Finalizing Products Page

--- frontend/src/landing_page/pricing/Hero.jsx
+++ frontend/src/landing_page/pricing/Hero.jsx
@@ -3,8 +3,31 @@ import './pricing.css'
 export default function Hero () {
     return (
         <>
-            <h2 className='mt-5 margin-top text-center' >Charges </h2>
-            <p>List of all charges and taxes</p>
+            <div>
+                <div className='text-center margin-top margin-bottom ' >
+                    <h1 className='mt-5 margin-top text-center' >Charges </h1>
+                    <p className='muted' >List of all charges and taxes</p>
+                </div>
+                <div className='flex' >
+                    <div className='img text-center '>
+                        <img src='pricing0.svg' alt='pricing1' style={{ width: '70%' }} />
+                        <h3>Free equity delivery</h3>
+                        <p className='text-muted small ' >All equity delivery investments (NSE, BSE), are absolutely free — ₹ 0 brokerage.</p>
+                    </div>
+                    <div className='img text-center ' >
+                        <img src='pricingIntra.png' alt='pricing2' style={{ width: '70%' }} />
+                        <h3>Intraday and F&O trades</h3>
+                        <p className='text-muted small ' >Flat ₹ 20 or 0.03% (whichever is lower) per executed order on intraday trades across equity,
+                            currency, and commodity trades. Flat ₹20 on all option trades.
+                        </p>
+                    </div>
+                    <div className='img text-center ' >
+                        <img src='pricingEquity.svg' alt='pricing2' style={{ width: '70%' }} />
+                        <h3>Free direct MF</h3>
+                        <p className='text-muted small ' >All direct mutual fund investments are absolutely free — ₹ 0 commissions & DP charges.</p>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
