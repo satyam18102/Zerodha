bb28566f87fbd7178afeaa342e504123ff90f9e0
Satyam Sinha <satyamsinha340@gmail.com>
2025-02-24 02:58:29 +0530

    Creating Dashboard

---
+++ Dashboard/src/Components/Dashboard.jsx
@@ -0,0 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+
+import Apps from './Apps.jsx'
+
+export default function Dashboard() {
+    return (
+        <div className="dashboard-container">
+            <GeneralContextProvider>
+                <WatchList />
+            </GeneralContextProvider>
+            <div className="content">
+                <Routes>
+                    <Route exact path="/" element={<Summary />} />
+                    <Route path="/orders" element={<Orders />} />
+                    <Route path="/holdings" element={<Holdings />} />
+                    <Route path="/positions" element={<Positions />} />
+                    <Route path="/funds" element={<Funds />} />
+                    <Route path="/apps" element={<Apps />} />
+                </Routes>
+            </div>
+        </div>
+    );
+}
