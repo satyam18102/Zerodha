4ab12c63f51516018a05ea2b12164e5db9946fa0
Satyam Sinha <satyamsinha340@gmail.com>
2025-02-23 18:48:12 +0530

    Creating Support Section

--- frontend/src/landing_page/support/Hero.jsx
+++ frontend/src/landing_page/support/Hero.jsx
@@ -5,16 +5,7 @@ export default function Hero (){
         <>
             <div className="bg-blue " >
                 <div className='margin-t flex' >
-                    <div className='img '>
-                        <h5 className='mb-5' >Support Portal</h5>
-                        <h5>Search for an answer or browse help topics to create a ticket</h5>
-                    </div>
-                    <div className='img-cont' >
-                        <a href="https://support.zerodha.com/login?next=https%3A%2F%2Fsupport.zerodha.com%2Ftickets%2F">
-                            Track tickets
-                        </a>
-                        <h5>Featured</h5>
-                    </div>
+                    
                 </div>
             </div>
         </>
