f78e9e4830d786b4b86d5efc93e1c185a6f6ec1d
Satyam Sinha <satyamsinha340@gmail.com>
2025-02-26 03:05:19 +0530

    Creating Backend

--- Dashboard/src/Components/Holdings.jsx
+++ Dashboard/src/Components/Holdings.jsx
@@ -1,9 +1,21 @@
-import {holdings} from "../data/data.js";
+import {useState , useEffect} from "react";
+
+import axios from "axios";
 
 export default function Holdings() {
+
+    const [allHoldings, setAllHoldings] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://localhost:8080/allHoldings").then((res) => {
+            setAllHoldings(res.data);
+        })
+    },[])
+
+
     return (
         <>
-            <h3 className="title">Holdings ({holdings.length})</h3>
+            <h3 className="title">Holdings ({allHoldings.length})</h3>
 
             <div className="order-table">
                 <table>
@@ -18,7 +30,7 @@ export default function Holdings() {
                         <th>Day chg.</th>
                     </tr>
 
-                    {holdings.map((stock, index) => {
+                    {allHoldings.map((stock, index) => {
                         const curValue = stock.price*stock.qty;
                         const isProfit = curValue-stock.avg*stock.qty >=0;
                         const profClass = isProfit ? 'profit' : 'loss';
