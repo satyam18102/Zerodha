3620a364da0cda581bc3b600d7426b0e437c537c
Satyam Sinha <satyamsinha340@gmail.com>
2025-02-25 14:57:20 +0530

    Finalizing Postions section

--- Dashboard/src/Components/Positions.jsx
+++ Dashboard/src/Components/Positions.jsx
@@ -25,13 +25,12 @@ export default function Positions() {
                         const dayClass = stock.isLoss ? 'loss' : 'profit';
                         return (
                             <tr key={index} >
+                                <td>{stock.product}</td>
                                 <td>{stock.name}</td>
                                 <td>{stock.qty}</td>
                                 <td>{stock.avg.toFixed(2)}</td>
                                 <td>{stock.price.toFixed(2)}</td>
-                                <td>{curValue}</td>
                                 <td className={profClass} >{(curValue-stock.avg*stock.qty).toFixed(2)}</td>
-                                <td className={profClass} >{stock.net}</td>
                                 <td className={dayClass} >{stock.day}</td>
                             </tr>
                         );
