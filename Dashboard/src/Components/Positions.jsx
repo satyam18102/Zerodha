ec3495525871b589fc698ba94886d6e0b16dc66d
Satyam Sinha <satyamsinha340@gmail.com>
2025-02-26 15:37:51 +0530

    Creating Backend

--- Dashboard/src/Components/Positions.jsx
+++ Dashboard/src/Components/Positions.jsx
@@ -1,9 +1,23 @@
-import {holdings, positions} from "../data/data.js";
+import {useState, useEffect} from 'react';
+
+import axios from 'axios';
+
+import {positions} from "../data/data.js";
 
 export default function Positions() {
+
+    const [allPositions, setAllPositions] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:8080/allPositions').then((res) => {
+            setAllPositions(res.data);
+        })
+    },[])
+
+
     return (
         <>
-            <h3 className="title">Positions ({positions.length})</h3>
+            <h3 className="title">Positions ({allPositions.length})</h3>
 
             <div className="order-table">
                 <table>
@@ -18,7 +32,7 @@ export default function Positions() {
                     </tr>
 
 
-                    {positions.map((stock, index) => {
+                    {allPositions.map((stock, index) => {
                         const curValue = stock.price*stock.qty;
                         const isProfit = curValue-stock.avg*stock.qty >=0;
                         const profClass = isProfit ? 'profit' : 'loss';
--- Dashboard/src/Components/TopBar.jsx
+++ Dashboard/src/Components/TopBar.jsx
@@ -7,12 +7,12 @@ export default function TopBar() {
                 <div className="indices-container">
                     <div className="nifty">
                         <p className="index">NIFTY 50</p>
-                        <p className="index-points">{100.2} </p>
+                        <p className="index-points">22,547 </p>
                         <p className="percent"> </p>
                     </div>
                     <div className="sensex">
                         <p className="index">SENSEX</p>
-                        <p className="index-points">{100.2}</p>
+                        <p className="index-points">74,602</p>
                         <p className="percent"></p>
                     </div>
                 </div>
