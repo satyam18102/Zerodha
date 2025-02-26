3d46633cdb2399f61115d1fe493277504523622a
Satyam Sinha <satyamsinha340@gmail.com>
2025-02-26 16:32:50 +0530

    Creating Backend

--- Dashboard/src/Components/BuyActionWindow.jsx
+++ Dashboard/src/Components/BuyActionWindow.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import {useState} from "react";
 import {Link} from 'react-router-dom';
 
 import axios from "axios";
@@ -11,7 +11,7 @@ export default function BuyActionWindow({uid}) {
     const [stockPrice, setStockPrice] = useState(0.0);
 
     const handleBuyClick = () => {
-        axios.post("http://localhost:3002/newOrder", {
+        axios.post("http://localhost:8080/newOrder", {
             name: uid,
             qty: stockQuantity,
             price: stockPrice,
